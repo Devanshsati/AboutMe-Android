84ea8a4e9c1848be62f36dcfcf264dd433c9fb1c
Devanshsati <thedevanshsati@gmail.com>
2023-12-19 21:11:33 +0530

    update binding

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,38 +1,43 @@
 package com.example.aboutme
 
-import android.content.Context
+//import android.content.Context
 import android.os.Bundle
-import android.view.View
-import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+//import android.view.View
+//import android.view.inputmethod.InputMethodManager
+//import android.widget.Button
+//import android.widget.EditText
+//import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 import com.example.aboutme.ui.theme.AboutMeTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.clickBtn).setOnClickListener { addNickName(it) }
+//        setContentView(R.layout.activity_main)
+//        findViewById<Button>(R.id.clickBtn).setOnClickListener { addNickName(it) }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
     }
 
-    private fun addNickName(view:View) {
-        val editText = findViewById<EditText>(R.id.inputText)
-        val nickNameTextView = findViewById<TextView>(R.id.viewText)
-
-        nickNameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nickNameTextView.visibility = View.VISIBLE
-
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
-    }
+//    private fun addNickName(view:View) {
+//        val editText = findViewById<EditText>(R.id.inputText)
+//        val nickNameTextView = findViewById<TextView>(R.id.viewText)
+//
+//        nickNameTextView.text = editText.text
+//        editText.visibility = View.GONE
+//        view.visibility = View.GONE
+//        nickNameTextView.visibility = View.VISIBLE
+//
+//        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//        imm.hideSoftInputFromWindow(view.windowToken, 0)
+//    }
 }
 
 @Composable
