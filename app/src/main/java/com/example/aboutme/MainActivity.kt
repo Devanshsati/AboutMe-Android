c66eec0eac9c4b926fda77ce7ff58784db85f3e4
Devanshsati <thedevanshsati@gmail.com>
2023-12-19 21:12:04 +0530

    update import

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.aboutme
 
-//import android.content.Context
 import android.os.Bundle
+//import android.content.Context
 //import android.view.View
 //import android.view.inputmethod.InputMethodManager
 //import android.widget.Button
