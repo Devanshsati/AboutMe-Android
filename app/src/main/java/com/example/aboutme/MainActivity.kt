a48c4374c8a62c0ee723a36919974f713d0927b3
Devanshsati <thedevanshsati@gmail.com>
2023-12-19 22:55:07 +0530

    fixed binding and removed imports fix bugs

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.example.aboutme
 
 import android.os.Bundle
-//import android.content.Context
-//import android.view.View
-//import android.view.inputmethod.InputMethodManager
-//import android.widget.Button
-//import android.widget.EditText
-//import android.widget.TextView
+import android.view.View
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.ComponentActivity
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,21 +20,20 @@ class MainActivity : ComponentActivity() {
 //        setContentView(R.layout.activity_main)
 //        findViewById<Button>(R.id.clickBtn).setOnClickListener { addNickName(it) }
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
+        binding.clickBtn.setOnClickListener { addNickName(it) }
     }
 
-//    private fun addNickName(view:View) {
-//        val editText = findViewById<EditText>(R.id.inputText)
-//        val nickNameTextView = findViewById<TextView>(R.id.viewText)
-//
-//        nickNameTextView.text = editText.text
-//        editText.visibility = View.GONE
-//        view.visibility = View.GONE
-//        nickNameTextView.visibility = View.VISIBLE
-//
-//        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-//        imm.hideSoftInputFromWindow(view.windowToken, 0)
-//    }
+    private fun addNickName(view: View) {
+        binding.apply {
+            viewText.text = inputText.text
+            invalidateAll()
+            inputText.visibility = View.GONE
+            clickBtn.visibility = View.GONE
+            viewText.visibility = View.VISIBLE
+        }
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
 }
 
 @Composable
